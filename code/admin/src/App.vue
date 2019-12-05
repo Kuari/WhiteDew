b2f5ac2d4af258a22ab8da18f198fec4b276a183
kuari <kuari@justmylife.cc>
2019-12-06 07:50:26 +0800

    开始后台管理界面开发

---
+++ code/admin/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+div.container {
+    margin: 30px;
+}
+</style>
---
+++ code/admin/src/views/dashboard/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="line1">
+            <panelGroup></panelGroup>
+        </div>
+        <div class="line2">
+            <el-row :gutter="20">
+                <el-col :span="12">
+                    <div class="panel">
+                        <pieChart></pieChart>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="panel">
+                        <!--<barchart></barchart>-->
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+import panelGroup from './components/panelGruop.vue'
+import pieChart from './components/PieChart.vue'
+
+export default {
+    name: 'Dashboard',
+    components: {
+        panelGroup,
+        pieChart
+    }
+}
+</script>
+
+<style scoped>
+.panel {
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    padding: 20px;
+    height: 400px;
+}
+.line1 {
+    margin-bottom: 20px;
+}
+</style>
